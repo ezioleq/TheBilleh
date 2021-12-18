5f0225e63073d816ed37ca0c73734eecbb8c3563
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-18 21:47:38 +0100

    Request Animation Frame

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { Vector2 } from "./math/vector";
 import { Player } from "./entities/player";
 
+var canvas: any;
+var ctx: CanvasRenderingContext2D;
+
+var keyboard: { [k: string]: any } = {};
+var player: Player;
+
 window.addEventListener("DOMContentLoaded", () => {
-	var canvas: any = document.getElementById("canvas");
-	var ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext('2d');
 
 	// init
-	var player = new Player(1280 / 2 - 50, 720 / 2 - 50);
-	var keyboard: { [k: string]: any } = {};
+	player = new Player(1280 / 2 - 50, 720 / 2 - 50);
 
 	window.addEventListener('keydown', (e) => {
 		keyboard[e.key.toLowerCase()] = true;
@@ -17,27 +22,25 @@ window.addEventListener("DOMContentLoaded", () => {
 		keyboard[e.key.toLowerCase()] = false;
 	});
 
-	// main loop
-	setInterval(() => {
-		// update
-		(() => {
-			player.update(keyboard);
-		})();
-
-		// draw
-		(() => {
-			// clear
-			ctx.fillStyle = "rgb(255, 255, 255)";
-			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			ctx.fillStyle = "rgb(0, 0, 0)";
-
-			player.draw(ctx);
-
-			ctx.font = "30px Roboto"
-			ctx.fillText(`Speed: ${player.moveSpeed}\n`, 10, 230);
-			ctx.fillText(`Dir: ${player.dir.toString()}\n`, 10, 260);
-			ctx.fillText(`Bullets: ${player.bullets.length}\n`, 10, 290);
-			ctx.fillText(`Vel mag: ${Math.round(player.vel.magnitude() * 100) / 100}`, 10, 320);
-		})();
-	}, 16);
+	window.requestAnimationFrame(update);
 });
+
+let update = () => {
+	// update
+	player.update(keyboard);
+
+	// draw
+	ctx.fillStyle = "rgb(255, 255, 255)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "rgb(0, 0, 0)";
+
+	player.draw(ctx);
+
+	ctx.font = "30px Roboto"
+	ctx.fillText(`Speed: ${player.moveSpeed}\n`, 10, 230);
+	ctx.fillText(`Dir: ${player.dir.toString()}\n`, 10, 260);
+	ctx.fillText(`Bullets: ${player.bullets.length}\n`, 10, 290);
+	ctx.fillText(`Vel mag: ${Math.round(player.vel.magnitude() * 100) / 100}`, 10, 320);
+
+	window.requestAnimationFrame(update);
+}
