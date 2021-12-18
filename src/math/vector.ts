e80010171f7dcd6dea7c7270845b0a8abab5a995
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-18 17:06:53 +0100

    Update Vector class
    Add cursed arithmetic methods
    Add method for calculating dot product
    Add move towards method with scuffed implementation
    Optimized usage of like 3 CPU instructions

--- src/math/vector.ts
+++ src/math/vector.ts
@@ -2,9 +2,41 @@ export class Vector2 {
 	x: number;
 	y: number;
 
+	readonly zero: Vector2 = new Vector2(0, 0);
+	readonly one: Vector2 = new Vector2(1, 1);
+	readonly up: Vector2 = new Vector2(0, 1);
+	readonly down: Vector2 = new Vector2(0, -1);
+	readonly right: Vector2 = new Vector2(1, 0);
+	readonly left: Vector2 = new Vector2(-1, 0);
+
 	constructor(x: number, y: number) {
 		this.x = x;
 		this.y = y;
+	}
+
+	// There is no operator overloading section
+	add(b: Vector2): Vector2 {
+		this.x += b.x;
+		this.y += b.y;
+		return this;
+	}
+
+	sub(b: Vector2): Vector2 {
+		this.x -= b.x;
+		this.y -= b.y;
+		return this;
+	}
+
+	mul(b: Vector2): Vector2 {
+		this.x *= b.x;
+		this.y *= b.y;
+		return this;
+	}
+
+	div(b: Vector2): Vector2 {
+		this.x /= b.x;
+		this.y /= b.y;
+		return this;
 	}
 
 	magnitude() {
@@ -14,9 +46,30 @@ export class Vector2 {
 	}
 
 	normalize() {
+		let magnitude = this.magnitude();
 		return new Vector2(
-			this.x / this.magnitude(),
-			this.y / this.magnitude()
+			this.x / magnitude,
+			this.y / magnitude
+		);
+	}
+
+	dot(b: Vector2) {
+		return this.x * b.x + this.y * b.y;
+	}
+
+	// Untested, may explode
+	move_towards(target: Vector2, max_distance_delta: number): Vector2 {
+		let a: Vector2 = new Vector2(this.x - target.x, this.y - target.y);
+		let magnitude = a.magnitude();
+
+		if (magnitude <= max_distance_delta || magnitude === 0)
+			return target;
+		
+		// Help
+		return new Vector2(this.x, this.y).add(
+			new Vector2(a.x / magnitude, a.y / magnitude)
+		).mul(
+			new Vector2(max_distance_delta, max_distance_delta)
 		);
 	}
 }
