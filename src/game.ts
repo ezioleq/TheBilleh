ac6af6762da0b84252e262e7f002a76bc415055b
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-20 17:06:41 +0100

    Add Game
    aka create Game class, now program entry point is clean.

---
+++ src/game.ts
@@ -0,0 +1,73 @@
+import { StateManager } from "./states/stateManager";
+import { GameState } from "./states/gameState";
+
+export class Game {
+	readonly gameWidth: number = 1280;
+	readonly gameHeight: number = 720;
+	stateManager: StateManager;
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D;
+
+	// Main loop arrow function hack
+	private mainLoop: any;
+
+	constructor() {
+		this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
+		this.ctx = this.canvas.getContext("2d");
+
+		// Set the canvas size to the in-game screen size
+		this.canvas.width = this.gameWidth;
+		this.canvas.height = this.gameHeight;
+
+		// Set the initial state
+		this.stateManager = new StateManager(new GameState());
+
+		// Handle window resizes
+		window.addEventListener("resize", this.handleResize);
+		window.dispatchEvent(new Event("resize"));
+
+		// Set our main loop
+		this.mainLoop = () => {
+			this.update();
+			this.draw();
+		}
+
+		// Start the game loop by requesting a frame
+		window.requestAnimationFrame(this.mainLoop);
+	}
+
+	handleResize() {
+		let screenWidth = window.innerWidth;
+		let screenHeight = window.innerHeight;
+
+		// Where should we put the black bars?
+		let horizontal: boolean =
+			(screenWidth / screenHeight) > (this.gameWidth / this.gameHeight);
+
+		if (horizontal) {
+			this.canvas.style.height = "100vh";
+			this.canvas.style.removeProperty("width");
+		} else {
+			this.canvas.style.width = "100vw";
+			this.canvas.style.removeProperty("height");
+		}
+	}
+
+	update() {
+		// Update current state
+		this.stateManager.getState().update();
+	}
+
+	draw() {
+		// Clear the screen
+		this.ctx.fillStyle = "rgb(255, 255, 255)";
+		this.ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+		this.ctx.fillStyle = "rgb(0, 0, 0)";
+
+		// Draw current state
+		this.stateManager.getState().draw(this.ctx, 1);
+
+		// Request a new frame
+		window.requestAnimationFrame(this.mainLoop);
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,5 @@
-import { StateManager } from "./states/stateManager";
-import { GameState } from "./states/gameState";
-
-var canvas: any;
-var ctx: CanvasRenderingContext2D;
-
-let gameWidth = 1280;
-let gameHeight = 720;
-
-let stateManager: StateManager = new StateManager();
+import { Game } from "./game";
 
 window.addEventListener("DOMContentLoaded", () => {
-	canvas = document.getElementById("canvas");
-	ctx = canvas.getContext('2d');
-
-	canvas.width = gameWidth;
-	canvas.height = gameHeight;
-
-	stateManager.setState(new GameState());
-
-	window.requestAnimationFrame(update);
-	window.dispatchEvent(new Event("resize"));
+	new Game();
 });
-
-window.addEventListener("resize", () => {
-	let screenWidth = window.innerWidth;
-	let screenHeight = window.innerHeight;
-
-	let horizontal: boolean = (screenWidth / screenHeight) > (gameWidth / gameHeight);
-
-	if (horizontal) {
-		canvas.style.height = "100vh";
-		canvas.style.removeProperty("width");
-	} else {
-		canvas.style.width = "100vw";
-		canvas.style.removeProperty("height");
-	}
-});
-
-let update = () => {
-	// update
-	stateManager.getState().update();
-
-	// draw
-	ctx.fillStyle = "rgb(255, 255, 255)";
-	ctx.fillRect(0, 0, gameWidth, gameHeight);
-	ctx.fillStyle = "rgb(0, 0, 0)";
-
-	stateManager.getState().draw(ctx, 1);
-
-	window.requestAnimationFrame(update);
-}
