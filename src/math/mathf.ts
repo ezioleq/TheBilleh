41cc6c1cdfd7a8ae8737d9ca5b829f330a65b5a1
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-23 19:45:19 +0100

    Add clamp method

--- src/math/mathf.ts
+++ src/math/mathf.ts
@@ -2,4 +2,8 @@ export class Mathf {
 	public static lerp(a: number, b: number, t: number) {
 		return (1 - t) * a + t * b;
 	}
+
+	public static clamp(n: number, min: number, max: number) {
+		return Math.min(Math.max(n, min), max);
+	}
 }
