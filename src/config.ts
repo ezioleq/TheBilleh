ca8361cec5896e113eea1f9179771dea07b164b0
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-12-24 16:46:50 +0100

    Add isDebug to Config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
+declare let process: any;
+const env = process.env.NODE_ENV;
+
 export const Config = {
 	gameWidth: 1280,
 	gameHeight: 720,
-	tps: 60
+	tps: 60,
+	isDebug: !(env === "production")
 };
