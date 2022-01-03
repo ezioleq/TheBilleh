bbb4528688ebfd74989e0d7e9e320437393173ce
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2022-01-03 22:29:26 +0100

    Mathf documentation

--- src/math/mathf.ts
+++ src/math/mathf.ts
@@ -1,9 +1,24 @@
+/**
+ * A set of some mathematical functions
+ */
 export class Mathf {
-	public static lerp(a: number, b: number, t: number) {
+	/**
+	 * @param a Start value
+	 * @param b End value
+	 * @param t Progress between 0 and 1
+	 * @returns Interpolated value, equals to `(1 - t) * a + t * b`
+	 */
+	public static lerp(a: number, b: number, t: number): number {
 		return (1 - t) * a + t * b;
 	}
 
-	public static clamp(n: number, min: number, max: number) {
+	/**
+	 * @param n Value
+	 * @param min Minimum value
+	 * @param max Maximum value
+	 * @returns Value between min and max
+	 */
+	public static clamp(n: number, min: number, max: number): number {
 		return Math.min(Math.max(n, min), max);
 	}
 }
