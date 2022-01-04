d563eb368cc7be27a9595090375ed0e11cc101fd
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2022-01-04 10:16:31 +0100

    Oh, there is a Map container in TypeScript
    And it was added in 2016 ... or 2015? Idk it was 12 years ago.

--- src/managers/assetManager.ts
+++ src/managers/assetManager.ts
@@ -1,8 +1,8 @@
 export class AssetManager {
 	private static _instance: AssetManager;
 
-	private textures: { [k: string]: HTMLImageElement } = {};
-	private sounds: { [k: string]: HTMLAudioElement } = {};
+	private textures: Map<string, HTMLImageElement> = new Map();
+	private sounds: Map<string, HTMLAudioElement> = new Map();
 
 	public static get Instance(): AssetManager {
 		return this._instance || (this._instance = new this());
@@ -12,22 +12,22 @@ export class AssetManager {
 		let texture: HTMLImageElement = new Image();
 		texture.src = path;
 
-		this.textures[name] = texture;
+		this.textures.set(name, texture);
 	}
 
 	public getTexture(name: string): HTMLImageElement {
-		return this.textures[name];
+		return this.textures.get(name);
 	}
 
 	public loadSound(name: string, path: string) {
 		let sound: HTMLAudioElement = new Audio();
 		sound.src = path;
 
-		this.sounds[name] = sound;
+		this.sounds.set(name, sound);
 	}
 
 	public getSound(name: string): HTMLAudioElement {
-		return this.sounds[name];
+		return this.sounds.get(name);
 	}
 }
 
