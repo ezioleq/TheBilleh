5856a2410033440014b7897f958b9a084901ed49
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2022-01-09 00:35:47 +0100

    Restore player movement

--- src/scripts/player.ts
+++ src/scripts/player.ts
@@ -1,8 +1,10 @@
-import { Assets, Component, Game, Rigidbody, SpriteRenderer, Vector2 } from "../mill";
+import { Assets, Component, Game, Input, Rigidbody, SpriteRenderer, Vector2 } from "../mill";
 
 export class Player extends Component {
 	private sr: SpriteRenderer = new SpriteRenderer();
 	private rb: Rigidbody = new Rigidbody();
+
+	private speed: number = 1;
 
 	override awake() {
 		this.entity.addComponent(this.sr);
@@ -25,6 +27,30 @@ export class Player extends Component {
 	}
 
 	override update() {
+		let moveDir = new Vector2(0, 0);
 
+		if (Input.pressed('w')) {
+			moveDir.y = -1;
+		}
+
+		if (Input.pressed('s')) {
+			moveDir.y = 1;
+		}
+
+		if (Input.pressed('a')) {
+			moveDir.x = -1;
+		}
+
+		if (Input.pressed('d')) {
+			moveDir.x = 1;
+		}
+
+		if (moveDir.magnitude() > 1) {
+			let normalized = moveDir.normalize();
+			moveDir.x = normalized.x;
+			moveDir.y = normalized.y;
+		}
+
+		this.rb.addForce(moveDir);
 	}
 }
